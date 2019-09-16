7170303c8bff8981724dfb979b1717617bea80f9
Nicholas <nchiassonpalacios@gmail.com>
2019-09-16 19:15:21 -0400

    Moved functional components out of character editor

--- src/CharacterEditor.js
+++ src/CharacterEditor.js
@@ -1,285 +1,34 @@
-import React, { useCallback } from "react";
-import app from "./base";
-import Copyright from './Copyright.js';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Logo from './assets/LogoWithText.png';
-import HostIcon from '@material-ui/icons/People';
-import Button from '@material-ui/core/Button';
-import Link from '@material-ui/core/Link';
-import TextField from '@material-ui/core/TextField';
-import useStyles from './Theme';
+import React from "react";
+import { CharacterEdit } from './FunctionalComponents';
 
 const firebase = require("firebase");
 require("firebase/firestore");
 
 var database = firebase.firestore();
 
-function CharacterEditor({ history }) {
-    const classes = useStyles();
+class CharacterEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      history: props.history,
+    };
+  }
 
-    const UpdateCharacter = useCallback(async event => {
-      event.preventDefault();
-      const { name, c_class, level, hp, 
-              strength, dexterity, constitution, intelligence, wisdom, charisma, 
-              initmod, ac, perception } = event.target.elements;
-      try {
-        await app.auth()   
-        const userId = app.auth().currentUser.uid;     
-        database.collection("characters").add({
-          userId: userId,
-          name: name.value,
-          c_class: c_class.value,
-          level: level.value,
-          hp: hp.value,
-          str: strength.value,
-          dex: dexterity.value,
-          con: constitution.value,
-          int: intelligence.value,
-          wis: wisdom.value,
-          cha: charisma.value,
-          initmod: initmod.value,
-          ac: ac.value,
-          perc: perception.value
-        })
-        history.push("/");
-      } catch(error) {
-        alert(error);
-      }
-    }, [history]
-    );
-
+  render()
+  {
+    if (this.state.loading) {
+      return (
+        <div> Loading </div>
+      );
+    }
     return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-          <Container>
-            <Link color="primary" href="/">
-              <img src ={Logo} alt = "Somebody yell at Nick" height = "60"/>
-            </Link>
-          </Container>
-          <Container align="right">
-            <Button
-            variant="outlined"
-            color="inherit"
-            onClick={() => app.auth().signOut()}
-            href = "/login"
-            >
-              <HostIcon /> &nbsp; Sign Out
-            </Button>
-          </Container>
-          </Toolbar>
-        </AppBar>
-        <main className = {classes.content}>
-          <div className = {classes.appBarSpacer} />
-            <Container maxWidth = "lg" className = {classes.container}>
-              <Paper className = {classes.paper}>
-                <form className={classes.form} noValidate onSubmit={UpdateCharacter}>
-                  <Grid container spacing={3} direction="row" justify="flex-start">
-                    <Grid item xs={6}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="name"
-                        label="Character Name"
-                        name="name"
-                        type = "text"
-                        autoComplete="Hugh Mann"
-                        autoFocus
-                      />
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={3} direction="row" justify="flex-start">
-                    <Grid item xs={2}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="c_class"
-                        label="Class"
-                        name="c_class"
-                        type = "text"
-                        autoComplete="Fighter"
-                      />
-                    </Grid>
-                    <Grid item xs={2}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="level"
-                        label="Level"
-                        name="level"
-                        type = "number"
-                        autoComplete="1"
-                      />
-                    </Grid>
-                    <Grid item xs={2}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="hp"
-                        label="Max Hit Points"
-                        name="hp"
-                        type = "number"
-                        autoComplete="1"
-                      />
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={3} direction="row" justify="flex-start">
-                    <Grid item xs={6} sm={1}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="strength"
-                        label="STR"
-                        name="strength"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={1}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="dexterity"
-                        label="DEX"
-                        name="dexterity"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={1}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="constitution"
-                        label="CON"
-                        name="constitution"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={1}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="intelligence"
-                        label="INT"
-                        name="intelligence"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={1}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="wisdom"
-                        label="WIS"
-                        name="wisdom"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={1}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="charisma"
-                        label="CHA"
-                        name="charisma"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={3} direction="row" justify="flex-start">
-                    <Grid item xs={6} sm={2}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="initmod"
-                        label="Initiative Modifier"
-                        name="initmod"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={2}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="ac"
-                        label="AC"
-                        name="ac"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                    <Grid item xs={6} sm={2}>
-                      <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        id="perception"
-                        label="Passive Perception"
-                        name="perception"
-                        type = "number"
-                        autoComplete="10"
-                      />
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={3} direction="row" justify="flex-start">
-                    <Grid item xs={6} sm={2}>
-                      <Button
-                        type="submit"
-                        variant="outlined"
-                        fullWidth
-                        color="default"
-                        className={classes.submit}
-                      >
-                        Save Character
-                      </Button>
-                    </Grid>
-                    <Grid item xs={6} sm={2}>
-                      <Button
-                        href= "/"
-                        type="button"
-                        variant="outlined"
-                        color="default"
-                        className={classes.submit}
-                      >
-                        Cancel
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </form>
-              </Paper>
-            </Container>
-          <Copyright />
-        </main>
+      <div>
+        <CharacterEdit history = {this.state.history}/>
       </div>
-    );
+    )
+  }
+    
 };
 
 export default CharacterEditor;
--- src/FunctionalComponents.js
+++ src/FunctionalComponents.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback } from "react";
 import app from "./base";
 import Copyright from './Copyright.js';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -25,6 +25,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 import useStyles from './Theme';
 import { IconButton } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 const firebase = require("firebase");
 require("firebase/firestore");
@@ -157,4 +158,267 @@ function CharacterList(props) {
   );
 }
 
-export { loadCharacters, CharacterList };
+function CharacterEdit({ history }) {
+    const classes = useStyles();
+
+    const UpdateCharacter = useCallback(async event => {
+      event.preventDefault();
+      const { name, c_class, level, hp, 
+              strength, dexterity, constitution, intelligence, wisdom, charisma, 
+              initmod, ac, perception } = event.target.elements;
+      try {
+        await app.auth()   
+        const userId = app.auth().currentUser.uid;     
+        database.collection("characters").add({
+          userId: userId,
+          name: name.value,
+          c_class: c_class.value,
+          level: level.value,
+          hp: hp.value,
+          str: strength.value,
+          dex: dexterity.value,
+          con: constitution.value,
+          int: intelligence.value,
+          wis: wisdom.value,
+          cha: charisma.value,
+          initmod: initmod.value,
+          ac: ac.value,
+          perc: perception.value
+        })
+        history.push("/");
+      } catch(error) {
+        alert(error);
+      }
+    }, [history]
+    );
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+          <Container>
+            <Link color="primary" href="/">
+              <img src ={Logo} alt = "Somebody yell at Nick" height = "60"/>
+            </Link>
+          </Container>
+          <Container align="right">
+            <Button
+            variant="outlined"
+            color="inherit"
+            onClick={() => app.auth().signOut()}
+            href = "/login"
+            >
+              <HostIcon /> &nbsp; Sign Out
+            </Button>
+          </Container>
+          </Toolbar>
+        </AppBar>
+        <main className = {classes.content}>
+          <div className = {classes.appBarSpacer} />
+            <Container maxWidth = "lg" className = {classes.container}>
+              <Paper className = {classes.paper}>
+                <form className={classes.form} noValidate onSubmit={UpdateCharacter}>
+                  <Grid container spacing={3} direction="row" justify="flex-start">
+                    <Grid item xs={6}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Character Name"
+                        name="name"
+                        type = "text"
+                        autoComplete="Hugh Mann"
+                        autoFocus
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={3} direction="row" justify="flex-start">
+                    <Grid item xs={2}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="c_class"
+                        label="Class"
+                        name="c_class"
+                        type = "text"
+                        autoComplete="Fighter"
+                      />
+                    </Grid>
+                    <Grid item xs={2}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="level"
+                        label="Level"
+                        name="level"
+                        type = "number"
+                        autoComplete="1"
+                      />
+                    </Grid>
+                    <Grid item xs={2}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="hp"
+                        label="Max Hit Points"
+                        name="hp"
+                        type = "number"
+                        autoComplete="1"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={3} direction="row" justify="flex-start">
+                    <Grid item xs={6} sm={1}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="strength"
+                        label="STR"
+                        name="strength"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={1}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="dexterity"
+                        label="DEX"
+                        name="dexterity"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={1}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="constitution"
+                        label="CON"
+                        name="constitution"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={1}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="intelligence"
+                        label="INT"
+                        name="intelligence"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={1}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="wisdom"
+                        label="WIS"
+                        name="wisdom"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={1}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="charisma"
+                        label="CHA"
+                        name="charisma"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={3} direction="row" justify="flex-start">
+                    <Grid item xs={6} sm={2}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="initmod"
+                        label="Initiative Modifier"
+                        name="initmod"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="ac"
+                        label="AC"
+                        name="ac"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                      <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        id="perception"
+                        label="Passive Perception"
+                        name="perception"
+                        type = "number"
+                        autoComplete="10"
+                      />
+                    </Grid>
+                  </Grid>
+                  <Grid container spacing={3} direction="row" justify="flex-start">
+                    <Grid item xs={6} sm={2}>
+                      <Button
+                        type="submit"
+                        variant="outlined"
+                        fullWidth
+                        color="default"
+                        className={classes.submit}
+                      >
+                        Save Character
+                      </Button>
+                    </Grid>
+                    <Grid item xs={6} sm={2}>
+                      <Button
+                        href= "/"
+                        type="button"
+                        variant="outlined"
+                        color="default"
+                        className={classes.submit}
+                      >
+                        Cancel
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </form>
+              </Paper>
+            </Container>
+          <Copyright />
+        </main>
+      </div>
+    );
+};
+
+export { loadCharacters, CharacterList, CharacterEdit };
